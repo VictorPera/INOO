e405e8baa4a56007a4380dc52d8eeb5fcacd29ff
VictorPera <victorhugo.pera@gmail.com>
2023-08-01 22:19:40 -0300

    Aula 01/08

---
+++ Aula 08-01/Exemplo1/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //declaração das variáveis
+        int number1, number2, sum;
+        number1 = 10;
+        number2 = 20;
+        sum = number1 + number2; 
+        
+        //saída
+        System.out.println("Soma = " + sum);
+        
+    }
+}
---
+++ Aula 08-01/PrimeiroProjeto/src/App.java
@@ -0,0 +1,8 @@
+//nome de classes são sempre com a "1° letra maiúscula"
+public class App {
+    //funções são chamadas de "métodos" e os -nomes- devem ser "minúsculos"
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //nada vem do nada em Java, portanto esse linha de código pega o "println(é escrito assim pois ja vem embutido no "print" o pular linha que é -\n- em C" que está ligado ao "objeto" -out- que está dentro da "classe" -System-
+    }
+}
